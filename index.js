5ce4217168125fdf140590468f61db78dddac63b
Santiago <santiburu77@gmail.com>
2025-07-23 12:28:56 -0400

    changes done

---
+++ index.js
@@ -0,0 +1,10 @@
+let code1 
+code1 = 7 + 3
+const code2 = (100 - 20) / 2
+const code3 = 39 % 47
+let message = "The Vault has been secured!.The combination is: "
+const codeA = code1 +  '-' + code2 + '-' + code3;
+const codeB = `${code1}, ${code2}, ${code3}`;
+console.log(message);
+console.log(codeA)
+console.log(codeB)
